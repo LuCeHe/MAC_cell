96775c7faf44c8e1181da9d6dfc76a27969b662e
Actla <manucelotti@gmail.com>
2018-11-07 17:57:15 -0500

    generator for training models on CLEVR ready

--- main.py
+++ main.py
@@ -9,25 +9,31 @@ Created on Tue Nov  6 23:17:58 2018
  - [DONE] read png
  - [DONE] read json
  - [DONE] read json line by line
- - build generator
- - get all the vocabulary
+ - [DONe] build generator
+ - [DONE] get all the vocabulary
  - tokenize generator
  - batches
+ - tokenize questions differently than answers
+ - [DONE] when tokenize, separate '?', ';', etc
 """
 
 import zipfile
 import imageio
 import json
 import logging
+import numpy as np
+import time
+from nlp import Vocabulary
+
 
 logger = logging.getLogger(__name__)
 
 
-def build_CLEVR_Vocabulary():
-    pass
 
 
-def CLEVR_generator(dataset_split = 'train', batchSize = 2):
+
+
+def CLEVR_generator(dataset_split = 'train', batchSize = 3):
     CLEVR_zip = zipfile.ZipFile("data/CLEVR_v1.0.zip", "r")
     
     if not dataset_split in ['train', 'test', 'val']:
@@ -43,50 +49,43 @@ def CLEVR_generator(dataset_split = 'train', batchSize = 2):
         data = f.read()  
         d = json.loads(data.decode("utf-8"))
        
-    print('Loaded')
-    print('Building Vocabulary')
-    vocabularyQuestions = []
-    vocabularyAnswers = []
     
-    i = 0 
-    for example in d['questions']:
-        i += 1
-        
-        question       = example['question']
-        answer         = example['answer']
-        
-        vocabularyQuestions += question.split()
-        vocabularyAnswers += answer.split()
-        
-        if i>=3: break
-        
-    print(vocabularyQuestions)
-    vocabularyQuestions = sorted(list(set(vocabularyQuestions)))
-    print(vocabularyQuestions)
-    print(vocabularyAnswers)
-    print('')
-    print(d.keys)
-    print('')
-    print(d['info'])
-    print('')
-    vocabularyComplete = []
+    vocabularyQuestions = Vocabulary.fromNpy('data/vocabularyQuestions.npy')
+    vocabularyAnswers = Vocabulary.fromNpy('data/vocabularyAnswers.npy')
     
     print('Generating Samples')    
+    
+    batches = {}
+    for key in ['images', 'questions', 'answers']:
+        batches[key] = []
+        
+        
     for example in d['questions']:
         
         image_filename = example['image_filename']
         image_filename = 'CLEVR_v1.0/images/' + dataset_split + '/' + image_filename
+        imgfile = CLEVR_zip.read(image_filename)
+        image = imageio.imread(imgfile)
+        batches['images'].append(image)
         
         question       = example['question']
+        question = vocabularyQuestions.sentenceToIndices(question)
+        question = np.array(question)
+        batches['questions'].append(question)
+        
         answer         = example['answer']
+        answer = vocabularyAnswers.sentenceToIndices(answer)
+        answer = np.array(answer)
+        batches['answers'].append(answer)
         
-        
-        print('')
-        print(image_filename)
-        print(question)
-        print(answer)
-        print('')
-        break
+        if len(batches['questions']) == batchSize:
+            images = np.array(batches['images'])
+            questions = np.array(batches['questions'])
+            answers = np.array(batches['answers'])
+            
+            for key in ['images', 'questions', 'answers']:
+                batches[key] = []
+            yield images, questions, answers
     
     
 
@@ -122,5 +121,29 @@ def test():
     print(d['info'])
 
 if __name__ == '__main__':
+    
+    
     #test()
-    CLEVR_generator()
+    #CLEVR_generator()
+    #build_CLEVR_Vocabulary()
+    begin = time.time()
+    generator = CLEVR_generator()
+    end = time.time()
+    LoadingTime = end - begin
+    print('it took %ds to load'%(LoadingTime))
+    
+    
+    nSamples = 3
+    TotSamplingTime = 0
+    for _ in range(nSamples):
+        begin = time.time()
+        batch = generator.__next__()
+        end = time.time()
+        SamplingTime = end - begin
+        TotSamplingTime += SamplingTime
+        print([element.shape for element in batch])
+        print('in %ds'%(SamplingTime)) 
+        print('')
+        
+    print('it took %ds/sample to sample'%(TotSamplingTime/nSamples))    
+    
---
+++ nlp.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 16:50:28 2018
+
+@author: celottil
+"""
+
+
+import zipfile
+import json
+import numpy as np
+from nltk.grammar import Nonterminal, CFG, Production
+
+
+class Vocabulary(object):
+    endToken = '<END>'
+    
+    def __init__(self, tokens):
+
+        if Vocabulary.endToken in tokens:
+            tokens.remove(Vocabulary.endToken)
+
+        indicesByTokens = dict()
+        tokens = [Vocabulary.endToken] + list(tokens)
+        for i, token in enumerate(tokens):
+            indicesByTokens[token] = i
+        self.__dict__.update(tokens=tokens,
+                             indicesByTokens=indicesByTokens)
+        
+    def __eq__(self, other):
+        return self.tokens == other.tokens
+
+    def __ne__(self, other):
+        return self.tokens != other.tokens
+
+    def __add__(self, other):
+        # NOTE: ignore the end token
+        tokens = set(self.tokens[1:])
+        tokens.update(other.tokens[1:])
+        return Vocabulary(list(sorted(tokens)))
+
+    def sort(self):
+        # NOTE: ignore the end token
+        tokens = sorted(self.tokens[1:])
+        self.indicesByTokens = dict()
+        self.tokens = [Vocabulary.endToken] + tokens
+        for i, token in enumerate(self.tokens):
+            self.indicesByTokens[token] = i
+
+    def indexToToken(self, idx):
+        return self.tokens[idx]
+
+    def indicesToTokens(self, indices, offset=0):
+        #print(indices)
+        #for i in indices:
+        #    print(i)
+        #    print(i, self.tokens[i-offset])
+        return [self.tokens[i-offset] for i in indices]
+
+    def tokenToIndex(self, token, offset=0):
+        return self.indicesByTokens[token] + offset
+
+    def tokensToIndices(self, tokens, offset=0):
+        indices = []
+        for token in tokens:
+            indices.append(self.indicesByTokens[token] + offset)
+        return indices
+
+    def sentenceToIndices(self, sentence, offset=0):
+        return self.tokensToIndices(sentenceSplit(sentence), offset)
+
+    def sentencesToIndices(self, sentences, offset=0):
+        indices = [self.sentenceToIndices(sentence, offset) for sentence in sentences]
+        return indices
+
+    def indicesToSentence(self, indices, offset=0):
+        return ' '.join(self.indicesToTokens(indices, offset))
+    
+    def indicesToSentences(self, indices_list, offset=0):
+        if type(indices_list).__module__ == 'numpy':
+            indices_list = indices_list.tolist()
+            
+            # unpad:
+            
+            indices_list = [list(filter((0).__ne__, indices)) for indices in indices_list]
+            
+        sentences = [self.indicesToSentence(indices, offset) for indices in indices_list]
+        return sentences
+
+    @staticmethod
+    def fromNltkGrammar(grammar):
+        tokens = []
+        for production in grammar.productions():
+            for p in production.rhs():
+                if not isinstance(p, Nonterminal):
+                    tokens.append(p)
+
+        # Remove redundant tokens and sort
+        tokens = list(set(tokens))
+        tokens.sort()
+
+        return Vocabulary(tokens)
+    
+    @staticmethod
+    def fromNpy(npy_filename = 'data/vocabularyComplete.npy'):
+        tokens = np.load(npy_filename)
+        return Vocabulary(tokens)
+
+
+def sentenceSplit(sentence):
+    split_sentence = sentence.split()
+    
+    punctuation = ['.', ';', '?']
+    
+    for p in punctuation:
+        is_p_contained = [p in token for token in split_sentence]
+        for i, item in enumerate(is_p_contained):
+            if item:
+                
+                word = split_sentence[i][:-1]
+                punctuation_simbol = split_sentence[i][-1]
+                
+                split_sentence[i] = word
+                split_sentence += [punctuation_simbol]
+    
+    return split_sentence
+    
+    
+def build_CLEVR_Vocabulary():
+    # TODO: get vocabulary from all datasets
+    CLEVR_zip = zipfile.ZipFile("data/CLEVR_v1.0.zip", "r")
+    
+    sets_names = ['train', 'val', 'test']
+    
+
+    #logger.info('\n\nLoading .json ')
+    
+    vocabularyQuestions = []
+    vocabularyAnswers = []
+    for set_name in sets_names:
+        
+        print('\nLoading .json %s'%(set_name))
+        
+        json_filename = 'CLEVR_v1.0/questions/CLEVR_%s_questions.json'%(set_name)
+        with CLEVR_zip.open(json_filename) as f:  
+            data = f.read()  
+            d = json.loads(data.decode("utf-8"))
+    
+        print('Building Vocabulary')
+        
+        i = 0 
+        for example in d['questions']:
+            i += 1
+            
+            question       = example['question']
+            vocabularyQuestions += sentenceSplit(question)
+            
+            try:
+                answer         = example['answer']
+                vocabularyAnswers += sentenceSplit(answer)
+            except KeyError:
+                pass
+            
+            #if i>=3: break
+        
+    vocabularyQuestions = sorted(list(set(vocabularyQuestions)))
+    vocabularyAnswers = sorted(list(set(vocabularyAnswers)))
+    vocabularyComplete = sorted(list(set(vocabularyQuestions + vocabularyAnswers)))
+    
+    np.save('data/vocabularyQuestions.npy', vocabularyQuestions)
+    np.save('data/vocabularyAnswers.npy', vocabularyAnswers)
+    np.save('data/vocabularyComplete.npy', vocabularyComplete)
+    
+    print('Finished!')
+    
+    
+if __name__ == '__main__':
+    build_CLEVR_Vocabulary()
+    
+    
